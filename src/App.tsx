506929a92fdeb506a4d3c9b589cf8f2de599b92e
hhjs2 <0092h@naver.com>
2023-11-21 22:47:22 +0900

    feat: 레이아웃 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import './App.css';
 import { Button } from '@material-tailwind/react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Main from './pages/main';
 
 function App() {
-	return <>
-	<div className="font-body text-primary text-title">안녕하세요</div>
-	<Button>small</Button>
-	<div className='lg bg-primary'>container</div>
-	</>
+	return (
+		<>
+			<div className="bg-bgGray h-screen">
+				<div className="bg-white h-screen w-[768px] px-4 pt-[56px] m-auto top-0 left-0">
+					<BrowserRouter>
+						<Routes>
+							<Route path="/" element={<Main />}></Route>
+						</Routes>
+					</BrowserRouter>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default App;
