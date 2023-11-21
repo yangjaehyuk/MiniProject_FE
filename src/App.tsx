392b17b53e81396eb0628a31fd3a5df69189d2f3
yangjaehyuk <gndksakxk10@naver.com>
2023-11-21 17:53:57 +0900

    Feat: 뒤로가기 기능 구현, 로그인 화면 헤더 UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,24 @@
 import { Route, Routes } from 'react-router';
 import React from 'react';
 import './App.css';
+import Login from 'pages/login';
 
 function App() {
 	return (
-
-		// <Routes> 주석빼고 본인 컴포넌트 넣어주세용
-		// 	<Route path='/' element={}/>
-		// 	<Route path='/:category' element={}/>
-		// 	<Route path='/:category/:region' element={}/>
-		// 	<Route path='/login' element={}/>
-		// 	<Route path='/signup' element={}/>
-		// 	<Route path='/mypage' element={}/>
-		// 	<Route path='/places/:hotelId' element={}/>
-		// 	<Route path='/places/:hotelId/:roomId' element={}/>
-		// 	<Route path='/cart' element={}/>
-		// 	<Route path='/orders' element={}/>
-		// 	<Route path='/result' element={}/>
-
-			
-		// </Routes>
-		<div><div>Hi</div></div> //지워주세요
-		
-	)
+		<Routes>
+			{/* // 	<Route path='/' element={}/> */}
+			{/* // 	<Route path='/:category' element={}/> */}
+			{/* // 	<Route path='/:category/:region' element={}/> */}
+			<Route path="/login" element={<Login />} />
+			{/* // 	<Route path='/signup' element={}/> */}
+			{/* // 	<Route path='/mypage' element={}/> */}
+			{/* // 	<Route path='/places/:hotelId' element={}/> */}
+			{/* // 	<Route path='/places/:hotelId/:roomId' element={}/> */}
+			{/* // 	<Route path='/cart' element={}/> */}
+			{/* // 	<Route path='/orders' element={}/> */}
+			{/* // 	<Route path='/result' element={}/> */}
+		</Routes>
+	);
 }
 
 export default App;
