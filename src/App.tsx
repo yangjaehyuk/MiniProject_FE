6e9e0551f203342e4a7899a1600f6e1dd94cb60c
jimin <moana16@naver.com>
2023-11-21 16:44:51 +0900

    feat : router 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
+import { Route, Routes } from 'react-router';
 import React from 'react';
 import './App.css';
-import { Button } from '@material-tailwind/react';
 
 function App() {
-	return <>
-	<div className="font-body text-primary text-title">안녕하세요</div>
-	<Button>small</Button>
-	<div className='lg bg-primary'>container</div>
-	</>
+	return (
+
+		// <Routes> 주석빼고 본인 컴포넌트 넣어주세용
+		// 	<Route path='/' element={}/>
+		// 	<Route path='/:category' element={}/>
+		// 	<Route path='/:category/:region' element={}/>
+		// 	<Route path='/login' element={}/>
+		// 	<Route path='/signup' element={}/>
+		// 	<Route path='/mypage' element={}/>
+		// 	<Route path='/places/:hotelId' element={}/>
+		// 	<Route path='/places/:hotelId/:roomId' element={}/>
+		// 	<Route path='/cart' element={}/>
+		// 	<Route path='/orders' element={}/>
+		// 	<Route path='/result' element={}/>
+
+			
+		// </Routes>
+		<div><div>Hi</div></div> //지워주세요
+		
+	)
 }
 
 export default App;
