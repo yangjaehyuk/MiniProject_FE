d615dae72ab56a9e0f02a64bc20a4b5fc11ed9c9
hhjs2 <0092h@naver.com>
2023-11-22 02:54:25 +0900

    feat: 메인 페이지 Ui 작업

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,8 @@ import Main from './pages/main';
 function App() {
 	return (
 		<>
-			<div className="bg-bgGray h-screen">
-				<div className="bg-white h-screen w-[768px] px-4 pt-[56px] m-auto top-0 left-0">
+			<div className="bg-bgGray h-full ">
+				<div className="bg-white h-screen w-[768px] px-4 pt-[56px] m-auto top-0 left-0 scrollbar-hide overflow-x-clip overflow-y-scroll ">
 					<BrowserRouter>
 						<Routes>
 							<Route path="/" element={<Main />}></Route>
--- src/components/main/header.tsx
+++ src/components/main/header.tsx
@@ -3,10 +3,10 @@ import DehazeOutlinedIcon from '@mui/icons-material/DehazeOutlined';
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 
-const header = () => {
+const Header = () => {
 	return (
-		<div className="bg-pink-200 fixed left-0 top-0 w-screen">
-			<div className="bg-white h-[56px] w-[768px] px-4 m-auto top-0 left-0 flex justify-between items-center ">
+		<div className=" bg-white fixed left-0 top-0 w-screen drop-shadow-sm">
+			<div className=" h-[56px] w-[768px] px-4 m-auto top-0 left-0 flex justify-between items-center ">
 				<DehazeOutlinedIcon fontSize="small" />
 				<div>
 					<div className="border-solid border-[1px] px-4 border-gray rounded-full w-[650px] flex justify-between items-center">
@@ -22,4 +22,4 @@ const header = () => {
 	);
 };
 
-export default header;
+export default Header;
