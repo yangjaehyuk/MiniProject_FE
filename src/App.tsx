d1d23560ccd56094a3a397cdc392d6eca79f7e53
skyeome <tjru7230@naver.com>
2023-11-22 14:36:23 +0900

    Hotfix: 레이아웃 수정

--- src/App.tsx
+++ src/App.tsx
@@ -7,26 +7,25 @@ import PlaceDetail from 'pages/placeDetail';
 import Login from 'pages/login';
 import Category from 'pages/category';
 import Region from 'pages/region';
+import Layout from 'components/common/Layout';
 
 function App() {
 	return (
-		<div className="bg-bgGray min-h-screen">
-			<div className="bg-white min-h-screen max-w-[768px] px-4 pt-[56px] m-auto top-0 left-0 overflow-x-clip">
-				<Routes>
-					<Route path="/" element={<Main />} />
-					<Route path="/:category" element={<Category />} />
-					<Route path="/:category/:region" element={<Region />} />
-					<Route path="/login" element={<Login />} />
-					{/* Route path="/signup" element={} />
+		<Routes>
+			<Route element={<Layout />}>
+				<Route path="/" element={<Main />} />
+				<Route path="/:category" element={<Category />} />
+				<Route path="/:category/:region" element={<Region />} />
+				<Route path="/login" element={<Login />} />
+				{/* Route path="/signup" element={} />
 				<Route path="/mypage" element={} /> */}
-					<Route path="/places/:hotelId" element={<PlaceDetail />} />
-					{/* <Route path="/places/:hotelId/:roomId" element={} /> */}
-					<Route path="/cart" element={<Cart />} />
-					{/* <Route path='/orders' element={}/>
+				<Route path="/places/:hotelId" element={<PlaceDetail />} />
+				{/* <Route path="/places/:hotelId/:roomId" element={} /> */}
+				<Route path="/cart" element={<Cart />} />
+				{/* <Route path='/orders' element={}/>
 					 <Route path='/result' element={}/> */}
-				</Routes>
-			</div>
-		</div>
+			</Route>
+		</Routes>
 	);
 }
 
