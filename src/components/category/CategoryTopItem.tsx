e74855d158e817c673352e82c1322b9c2029d008
skyeome <tjru7230@naver.com>
2023-11-22 18:13:35 +0900

    Feat: 평점 TOP 숙소 추가중

--- src/components/category/CategoryTopItem.tsx
+++ src/components/category/CategoryTopItem.tsx
@@ -6,24 +6,30 @@ import { formatNumberWithCommas } from 'utils/numberComma';
 function CategoryTopItem({ image, price, rating, title }: ProductItem) {
 	return (
 		<div className="border border-borderGray rounded-md overflow-hidden">
-			<div className="item-image">
-				<img src={image} alt={title} />
-			</div>
-			<div className="item-desc p-3">
-				<h4 className="text-sm lg:text-[1rem]">{title}</h4>
-				<p>
-					<Star
-						sx={{
-							color: '#FFE500',
-							fontSize: '0.875rem',
-							verticalAlign: 'initial',
-						}}
-					/>{' '}
-					{rating}
-				</p>
-				<h5 className="text-right text-title font-semibold">
-					{formatNumberWithCommas(price)}원~
-				</h5>
+			<div className="flex">
+				<div className="w-4/12">
+					<div className="item-image">
+						<img src={image} alt={title} />
+					</div>
+				</div>
+				<div className="w-8/12">
+					<div className="item-desc p-3">
+						<h4 className="text-sm lg:text-[1rem]">{title}</h4>
+						<p>
+							<Star
+								sx={{
+									color: '#FFE500',
+									fontSize: '0.875rem',
+									verticalAlign: 'initial',
+								}}
+							/>{' '}
+							{rating}
+						</p>
+						<h5 className="text-right text-title font-semibold">
+							{formatNumberWithCommas(price)}원~
+						</h5>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
--- src/components/category/CategoryTopRated.tsx
+++ src/components/category/CategoryTopRated.tsx
@@ -5,7 +5,12 @@ import CategoryTopItem from './CategoryTopItem';
 function CategoryTopRated({ title, items }: ProductItemsProps) {
 	return (
 		<div className="py-5 mt-5">
-			<h2 className="text-title font-semibold mb-4">{title}</h2>
+			<div className="mb-4">
+				<h2 className="text-title font-semibold">{title}</h2>
+				<p className="text-content text-textGray">
+					고객분들의 좋은 평점 후기를 받은 상품
+				</p>
+			</div>
 			<div className="grid gap-4 grid-cols-1 lg:grid-cols-2">
 				{items.map((item) => (
 					<CategoryTopItem key={item.id} {...item} />
