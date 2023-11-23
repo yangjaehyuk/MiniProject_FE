a3cc14305883f1e1506c08a52205e67442dbb412
yangjaehyuk <gndksakxk10@naver.com>
2023-11-23 20:15:57 +0900

    Feat: 장바구니 Footer 사이즈 늘어나는 기능

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -125,8 +125,8 @@ const Cart = () => {
 					</div>
 				</div>
 			</div>
-			<div className="bg-white h-[110px] shadow-inner w-screen fixed bottom-0  left-0">
-				<div className="w-[768px] m-auto top-0 left-0">
+			<div className="bg-white ${show ? 'h-[150px]' : 'h-[110px]'} shadow-inner w-screen fixed bottom-0  left-0">
+				<div className="w-[768px] m-auto top-0 left-0 pb-3">
 					<div className="flex justify-between items-center py-2">
 						<div className="font-semibold  text-sm"> 총 1건</div>
 						<div className="flex items-center">
@@ -139,7 +139,7 @@ const Cart = () => {
 						예약하기
 					</button>
 					{show && (
-						<div className="text-textGray text-xxsm mt-2">
+						<div className="text-textGray text-xxsm mt-2 ">
 							(주)야놀자는 통신판매중개업자로서, 통신판매의 당사자가 아니라는
 							사실을 고지하며 상품의 예약, 이용 및 환불 등과 관련한 의무와
 							책임은 각 판매자에게 있습니다.
