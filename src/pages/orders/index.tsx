c65fa6360bf88b7972490b0d4e391746f38309c3
hhjs2 <0092h@naver.com>
2023-11-24 02:32:45 +0900

    feat: checkBox 약관 동의 로직 구현

--- src/pages/orders/index.tsx
+++ src/pages/orders/index.tsx
@@ -17,6 +17,7 @@ export type ReservationInfo = {
 	userName: string;
 	userNumber: string;
 	paymentMethod?: string;
+	selectAllCheckbox: boolean;
 	requiredCheckbox1?: boolean;
 	requiredCheckbox2?: boolean;
 	requiredCheckbox3?: boolean;
@@ -30,6 +31,7 @@ const orders = () => {
 	const onSubmit = (data: any) => console.log('ddd');
 
 	const [payment, setPayment] = useState('');
+	const [checkbox, setCheckbox] = useState(false);
 
 	const handlePayment = (newPayment: string) => {
 		setPayment(newPayment);
@@ -37,6 +39,8 @@ const orders = () => {
 	};
 
 	console.log(payment);
+	console.log('checkbox: ', checkbox);
+
 	return (
 		<>
 			<FormProvider {...methods}>
@@ -67,18 +71,24 @@ const orders = () => {
 				</div>
 				<div className={styles.wrap}>
 					<PaymentNotice />
-					<TermsAgreement />
+					<TermsAgreement setCheckbox={setCheckbox} />
 					<div>
 						<div className="text-textGray text-xxsm mt-2 py-2">
 							이용규칙, 취소 및 환불 규칙 동의하실 경우 결제하기를 클릭해주세요.
 						</div>
 						<form onSubmit={methods.handleSubmit(onSubmit)}>
-							<button
-								type="submit"
-								className="flex font-semibold text-content justify-center items-center w-full py-5 text-center bg-secondary rounded-md h-[20px]  text-white"
-							>
-								870,000원 결제하기
-							</button>
+							{checkbox ? (
+								<button className="flex font-semibold text-content justify-center items-center w-full py-5 text-center bg-secondary rounded-md h-[20px]  text-white">
+									870,000원 결제하기
+								</button>
+							) : (
+								<button
+									disabled
+									className="flex font-semibold text-content justify-center items-center w-full py-5 text-center bg-textGray rounded-md h-[20px]  text-white"
+								>
+									870,000원 결제하기
+								</button>
+							)}
 						</form>
 						<div className="text-textGray text-xxsm mt-2">
 							(주)야놀자는 통신판매중개업자로서, 통신판매의 당사자가 아니라는
