e61cfd353416892d2f166a522a6a979b2f390f9b
hhjs2 <0092h@naver.com>
2023-11-24 01:06:47 +0900

    feat: 결제 수단 반환 로직 구현

--- src/components/orders/PaymentMethod.tsx
+++ src/components/orders/PaymentMethod.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Ssg from '../../assets/images/ssg.svg';
 import KB from '../../assets/images/kb.svg';
 import Naver from '../../assets/images/naver.svg';
@@ -8,8 +8,25 @@ import Payco from '../../assets/images/payco.svg';
 import Smile from '../../assets/images/smile.svg';
 import Lpay from '../../assets/images/L.pay.svg';
 import banner from '../../assets/images/bannerKakao.svg';
+import { useFormContext } from 'react-hook-form';
 
-const PaymentMethod = () => {
+type PaymentMethodProps = {
+	handlePayment: (newPayment: string) => void;
+};
+
+const PaymentMethod: React.FC<PaymentMethodProps> = ({ handlePayment }) => {
+	const {
+		register,
+		formState: { errors },
+	} = useFormContext();
+
+	const [selectedPayment, setSelectedPayment] = useState('');
+
+	const handlePaymentClick = (paymentName: string) => {
+		setSelectedPayment(paymentName);
+		handlePayment(paymentName);
+		console.log('selectedPayment', selectedPayment);
+	};
 	return (
 		<>
 			<div>
@@ -17,42 +34,86 @@ const PaymentMethod = () => {
 			</div>
 			<div className="font-semibold text-content pb-2 pb-5">결제 수단</div>
 			<div className="flex flex-col ">
-				<button className=" flex items-center justify-center border-solid border-[1px] h-[48px] border-gray rounded-sm ">
+				<button
+					onClick={() => handlePaymentClick('카카오페이')}
+					type="button"
+					className=" flex items-center justify-center border-solid border-[1px] h-[48px] border-gray rounded-sm "
+				>
 					<img src={Kakao} className="h-6" alt="Kakao Logo" />
 				</button>
 				<div className="flex justify-between items-center ">
 					<div className="flex flex-col w-1/2 ">
-						<button className=" mt-3 flex items-center justify-center mr-3  border-solid border-[1px] h-[48px] border-gray rounded-sm ">
+						<button
+							onClick={() => handlePaymentClick('네이버페이')}
+							type="button"
+							className=" mt-3 flex items-center justify-center mr-3  border-solid border-[1px] h-[48px] border-gray rounded-sm "
+						>
 							<img src={Naver} className="h-6" alt="Kakao Logo" />
 						</button>
 
-						<button className=" mt-3 flex items-center justify-center mr-3 border-solid border-[1px] h-[48px] border-gray rounded-sm ">
+						<button
+							onClick={() => handlePaymentClick('카드')}
+							type="button"
+							className=" mt-3 flex items-center justify-center mr-3 border-solid border-[1px] h-[48px] border-gray rounded-sm "
+						>
 							<div className="text-sm"> 카드 </div>
 						</button>
-						<button className="mt-3 flex items-center justify-center mr-3 border-solid border-[1px] h-[48px] border-gray rounded-sm ">
+						<button
+							onClick={() => handlePaymentClick('페이코')}
+							type="button"
+							className="mt-3 flex items-center justify-center mr-3 border-solid border-[1px] h-[48px] border-gray rounded-sm "
+						>
 							<img src={Payco} className="h-5" alt="Kakao Logo" />
 						</button>
-						<button className="mt-3 flex items-center justify-center mr-3 border-solid border-[1px] h-[48px] border-gray rounded-sm ">
+						<button
+							onClick={() => handlePaymentClick('스마일페이')}
+							type="button"
+							className="mt-3 flex items-center justify-center mr-3 border-solid border-[1px] h-[48px] border-gray rounded-sm "
+						>
 							<img src={Smile} className="h-6" alt="Kakao Logo" />
 						</button>
-						<button className="mt-3 flex items-center justify-center mr-3 border-solid border-[1px] h-[48px] border-gray rounded-sm ">
+						<button
+							onClick={() => handlePaymentClick('L.PAY')}
+							type="button"
+							className="mt-3 flex items-center justify-center mr-3 border-solid border-[1px] h-[48px] border-gray rounded-sm "
+						>
 							<img src={Lpay} className="h-5" alt="Kakao Logo" />
 						</button>
 					</div>
 					<div className="flex flex-col w-1/2">
-						<button className="mt-3 flex items-center justify-center ml-3 border-solid border-[1px] h-[48px] border-gray rounded-sm ">
+						<button
+							onClick={() => handlePaymentClick('토스페이')}
+							type="button"
+							className="mt-3 flex items-center justify-center ml-3 border-solid border-[1px] h-[48px] border-gray rounded-sm "
+						>
 							<img src={Toss} className="h-5" alt="Kakao Logo" />
 						</button>
-						<button className="mt-3 flex items-center justify-center ml-3 border-solid border-[1px] h-[48px] border-gray rounded-sm ">
+						<button
+							onClick={() => handlePaymentClick('KB 페이')}
+							type="button"
+							className="mt-3 flex items-center justify-center ml-3 border-solid border-[1px] h-[48px] border-gray rounded-sm "
+						>
 							<img src={KB} className="h-6" alt="Kakao Logo" />
 						</button>
-						<button className="mt-3 flex items-center justify-center ml-3 border-solid border-[1px] h-[48px] border-gray rounded-sm ">
+						<button
+							onClick={() => handlePaymentClick('휴대폰')}
+							type="button"
+							className="mt-3 flex items-center justify-center ml-3 border-solid border-[1px] h-[48px] border-gray rounded-sm "
+						>
 							<div className="text-sm"> 휴대폰 </div>
 						</button>
-						<button className="mt-3 flex items-center justify-center ml-3 border-solid border-[1px] h-[48px] border-gray rounded-sm ">
+						<button
+							onClick={() => handlePaymentClick('SSG페이')}
+							type="button"
+							className="mt-3 flex items-center justify-center ml-3 border-solid border-[1px] h-[48px] border-gray rounded-sm "
+						>
 							<img src={Ssg} className="h-5" alt="Kakao Logo" />
 						</button>
-						<button className="mt-3 flex items-center justify-center ml-3 border-solid border-[1px] h-[48px] border-gray rounded-sm ">
+						<button
+							onClick={() => handlePaymentClick('실시간계좌이체')}
+							type="button"
+							className="mt-3 flex items-center justify-center ml-3 border-solid border-[1px] h-[48px] border-gray rounded-sm "
+						>
 							<div className="text-sm"> 실시간계좌이체 </div>
 						</button>
 					</div>
