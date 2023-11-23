ba59406de6eec72641457c304b330a546e22b596
hhjs2 <0092h@naver.com>
2023-11-23 13:25:42 +0900

    feat : ResultFooter 구현

--- src/pages/result/index.tsx
+++ src/pages/result/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import Header from 'components/common/Header';
-import InfoIcon from '@mui/icons-material/Info';
 import styles from '../../components/cart/Cart.module.css';
-import carImage from '../../assets/images/cart-img.png';
+
 import ExpandMoreOutlinedIcon from '@mui/icons-material/ExpandMoreOutlined';
+import ResultFooter from 'components/Footer/ResultFooter';
 const result = () => {
 	return (
 		<div>
@@ -112,19 +112,7 @@ const result = () => {
 					홈으로
 				</button>
 			</div>
-			<div className="bg-white h-[110px] shadow-inner w-screen fixed bottom-0  left-0">
-				<div className="w-[768px] m-auto top-0 left-0">
-					<div className="mt-4">
-						<img src={carImage} className="rounded-md mb-2" />
-					</div>
-
-					<div className="text-textGray text-xxsm mt-2">
-						(주)야놀자는 통신판매중개업자로서, 통신판매의 당사자가 아니라는
-						사실을 고지하며 상품의 예약, 이용 및 환불 등과 관련한 의무와 책임은
-						각 판매자에게 있습니다.
-					</div>
-				</div>
-			</div>
+			<ResultFooter />
 		</div>
 	);
 };
