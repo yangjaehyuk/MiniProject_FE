668951c4a9badf39cdd1e674301033eb179aab22
yangjaehyuk <gndksakxk10@naver.com>
2023-11-27 01:37:11 +0900

    Fix: 리다이렉션 로직 hook 처리

---
+++ src/components/common/topBtn.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import TopSign from '../../assets/images/top.svg';
+import BtnForm from '../../assets/images/btn.svg';
+
+const TopBtn = () => {
+	return (
+		<button>
+			<img src={TopSign} alt="Top Sign" />
+		</button>
+	);
+};
+
+export default TopBtn;
