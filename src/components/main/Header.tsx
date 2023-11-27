800297bde2a95f1cde77e1ad59c5c381449234bb
yangjaehyuk <gndksakxk10@naver.com>
2023-11-27 23:50:40 +0900

    Fix: main Header 추가

---
+++ src/components/main/Header.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import DehazeOutlinedIcon from '@mui/icons-material/DehazeOutlined';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import { Link } from 'react-router-dom';
+import { MainHeaderProps } from 'types/MainPage.type';
+
+const Header = ({ handleOpen }: MainHeaderProps) => {
+	return (
+		<div className=" bg-white fixed left-0 top-0 w-screen drop-shadow-sm ">
+			<div className=" h-[56px] w-[768px] px-4 m-auto top-0 left-0 flex justify-between items-center ">
+				<div className="cursor-pointer" onClick={handleOpen}>
+					<DehazeOutlinedIcon fontSize="small" />
+				</div>
+				<div>
+					<div className="border-solid border-[1px] px-4 border-gray rounded-full w-[650px] flex justify-between items-center">
+						<span className=" text-textGray text-sm py-1.5">
+							무엇을 하고 놀까요 ?
+						</span>
+						<SearchOutlinedIcon fontSize="small" />
+					</div>
+				</div>
+				<Link to="/cart">
+					<ShoppingCartOutlinedIcon fontSize="small" />
+				</Link>
+			</div>
+		</div>
+	);
+};
+
+export default Header;
