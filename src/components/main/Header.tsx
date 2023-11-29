28f35a68f53bd028ae353b157efb0c0675ecb947
skyeome <tjru7230@naver.com>
2023-11-29 16:32:41 +0900

    Feat: 카트 갯수 헤더에서 표시

--- src/components/main/Header.tsx
+++ src/components/main/Header.tsx
@@ -4,22 +4,24 @@ import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 import { Link, useNavigate } from 'react-router-dom';
 import { MainHeaderProps } from 'types/MainPage.type';
+import { useQueryCartCount } from 'hooks/common/useQueryCartCount';
+import { Badge } from '@material-tailwind/react';
 
 const Header = ({ handleOpen }: MainHeaderProps) => {
 	const navigate = useNavigate();
+	const { data: cartCount } = useQueryCartCount();
 
 	const handleInputBtn = () => {
 		navigate('/search');
-
-	}
+	};
 	return (
 		<div className=" bg-white fixed left-0 top-0 w-screen drop-shadow-sm  z-10">
 			<div className=" h-[56px] w-[768px] px-4 m-auto top-0 left-0 flex justify-between items-center ">
 				<div className="cursor-pointer" onClick={handleOpen}>
 					<DehazeOutlinedIcon fontSize="small" />
 				</div>
-				<div onClick={handleInputBtn} >
-					<div  className="border-solid border-[1px] px-4 border-gray rounded-full w-[650px] flex justify-between items-center cursor-pointer">
+				<div onClick={handleInputBtn}>
+					<div className="border-solid border-[1px] px-4 border-gray rounded-full w-[650px] flex justify-between items-center cursor-pointer">
 						<span className=" text-textGray text-sm py-1.5">
 							무엇을 하고 놀까요 ?
 						</span>
@@ -27,7 +29,16 @@ const Header = ({ handleOpen }: MainHeaderProps) => {
 					</div>
 				</div>
 				<Link to="/cart">
-					<ShoppingCartOutlinedIcon fontSize="small" />
+					{cartCount && cartCount > 0 ? (
+						<Badge
+							content={cartCount}
+							className="bg-primary p-0 w-4 min-w-0 h-4 min-h-0"
+						>
+							<ShoppingCartOutlinedIcon fontSize="small" />
+						</Badge>
+					) : (
+						<ShoppingCartOutlinedIcon fontSize="small" />
+					)}
 				</Link>
 			</div>
 		</div>
