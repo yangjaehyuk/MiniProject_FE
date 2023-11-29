a0d5fb39e7cf344f3855ef8194247555f84f75b3
yangjaehyuk <gndksakxk10@naver.com>
2023-11-30 01:12:03 +0900

    Test: signUp test

---
+++ test/jest signup.test.ts
@@ -0,0 +1,18 @@
+import supertest from 'supertest';
+const request = supertest('https://api.hitit7mini.store');
+
+test('POST /api/members/signup', async () => {
+	const userData = {
+		email: 'emailaaa@hanmail.net',
+		password: 'password123!',
+		name: '마이콜',
+	};
+
+	const response = await request
+		.post('/api/members/signup')
+		.set('Content-Type', 'application/json')
+		.send(userData);
+
+	expect(response.status).toEqual(201); // success
+	// expect(response.status).toEqual(401); // duplicated or error
+});
